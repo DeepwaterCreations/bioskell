bb46cc4a1d659630d54483035869044dd9e45a7a
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-01-25 16:05:48 -0700

    Reverse patternToNumber parse direction
    This way, it will return the same values that the book expects. (It
    works fine either way, but this might make later stuff easier to check
    against expected results.)

--- bioskell.hs
+++ bioskell.hs
@@ -10,11 +10,10 @@ patternCount string@(x:xs) pattern
             | otherwise = patternMatch ss ps
 
 -- Transforms a string of k DNA nucleotides into an integer between 0 and 4^k-1
--- Currently reads the string backwards from how the book does it.
 patternToNumber :: String -> Int
-patternToNumber pattern@(x:xp)
-    |null xp = baseTo4git x
-    |otherwise = (4 * patternToNumber xp) + baseTo4git x
+patternToNumber pattern
+    |null (init pattern) = baseTo4git (last pattern)
+    |otherwise = (4 * patternToNumber (init pattern)) + baseTo4git (last pattern)
     where baseTo4git 'a' = 0
           baseTo4git 'c' = 1
           baseTo4git 'g' = 2
