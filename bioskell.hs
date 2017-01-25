6db8837894fbe2d58c32b359b6ab099e9deb3dc1
DeepwaterCreations <dylan.craine@deepwatercreations.com>
2017-01-25 00:44:46 -0700

    Implement patternToNumber (p39)

--- bioskell.hs
+++ bioskell.hs
@@ -8,3 +8,15 @@ patternCount string@(x:xs) pattern
             | null ps = 1
             | null ss = 0
             | otherwise = patternMatch ss ps
+
+-- Transforms a string of k DNA nucleotides into an integer between 0 and 4^k-1
+-- Currently reads the string backwards from how the book does it.
+patternToNumber :: String -> Int
+patternToNumber pattern@(x:xp)
+    |null xp = baseTo4git x
+    |otherwise = (4 * patternToNumber xp) + baseTo4git x
+    where baseTo4git 'a' = 0
+          baseTo4git 'c' = 1
+          baseTo4git 'g' = 2
+          baseTo4git 't' = 3
+          baseTo4git c = error ("Character '" ++ c:[] ++ "' not in dictionary")
